4f81b30d1a563a8af5d31d8303e1af4d56309c4c
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-24 14:23:58 +0530

    new

---
+++ src/main/java/com/example/demo/EduvibeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduvibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduvibeApplication.class, args);
+	}
+
+}
