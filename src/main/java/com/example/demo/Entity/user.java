b56182a7719d9abc0c0ef60b0e625dec7e1113e2
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-24 15:03:16 +0530

    changes

--- src/main/java/com/example/demo/Entity/user.java
+++ src/main/java/com/example/demo/Entity/user.java
@@ -1,9 +1,6 @@
 package com.example.demo.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -16,8 +13,13 @@ public class user {
     private int id;
     private String username;
     private String password;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Enumerated(EnumType.STRING)
     private Role role;
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
     private LocalDateTime createdAt;
 
 }
