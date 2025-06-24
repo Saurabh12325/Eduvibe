996eb1447f411a02f0daa0031b1c4499a5394fdb
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-24 14:53:55 +0530

    changes

--- src/main/java/com/example/demo/Entity/Role.java
+++ src/main/java/com/example/demo/Entity/Role.java
@@ -1,4 +1,6 @@
 package com.example.demo.Entity;
 
-public class Role {
+public enum Role {
+    Teacher,
+    Student
 }
--- src/main/java/com/example/demo/Entity/user.java
+++ src/main/java/com/example/demo/Entity/user.java
@@ -1,4 +1,23 @@
 package com.example.demo.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private Role role;
+    private LocalDateTime createdAt;
+
 }
