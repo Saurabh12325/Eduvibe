e324ec27a8ca473a05351850eb9d81b9e6fd6141
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-24 15:55:46 +0530

    changes

--- src/main/java/com/example/demo/Service/EmailService/OtpService.java
+++ src/main/java/com/example/demo/Service/EmailService/OtpService.java
@@ -1,4 +1,15 @@
 package com.example.demo.Service.EmailService;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
 public class OtpService {
+
+    private String generateOtp() {
+        Random random = new Random();
+        return String.format("%06d", random.nextInt(10000));
+    }
+
 }
